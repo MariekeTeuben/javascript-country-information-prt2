361bd10fc707a44879a143bda783975725efde0b
MariekeTeuben <marieke@marenmaa.nl>
2022-08-31 15:09:05 +0200

    Zoekfunctionaliteit toegevoegd

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -462,11 +462,19 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
-async function getCountryInformation() {
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', searchCountry);
+function searchCountry(e) {
+    e.preventDefault();
+    const searchField = document.getElementById('search-field');
+    getCountryInformation(searchField.value);
+    searchField.value = '';
+}
+async function getCountryInformation(name) {
     try {
-        const result = await _axiosDefault.default.get('https://restcountries.com/v2/name/nederland');
+        const result = await _axiosDefault.default.get(`https://restcountries.com/v2/name/${name}`);
         const countryData = result.data[0];
-        //console.log(countryData);
+        console.log(result.data[0]);
         createCountryInfo(countryData);
     } catch (e) {
         console.error(e);
@@ -475,12 +483,23 @@ async function getCountryInformation() {
 getCountryInformation();
 function createCountryInfo(countryData) {
     const infoParagraph = document.getElementById('country-information');
+    console.log(countryData);
     infoParagraph.innerHTML = `
     <p>${countryData.name}</p>
     <img src="${countryData.flag}" alt="Vlag van ${countryData.name}" class="flag" />
     <p>${countryData.name} is situated in ${countryData.subregion}. It has a population of ${countryData.population} people.</p>
+    <p>The capital is ${countryData.capital} ${currencyDescription(countryData.currencies)}</p>
     `;
 }
+function currencyDescription(currencies) {
+    let output = 'and you can pay with ';
+    if (currencies.length === 2) return output + `${currencies[0].name} and ${currencies[1].name}`;
+    return output + `${currencies[0].name}`;
+}
+const form = document.getElementById("search-form");
+form.addEventListener("submit", ()=>{
+    createCountryInfo();
+});
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
